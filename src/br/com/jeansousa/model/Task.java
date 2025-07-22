c8dedd7fca2197754c83a1a088330a590ee808e1
Jean Sousa <jjeanssousa16@gmail.com>
2025-07-22 09:36:16 -0300

    criar variaveis da entidade Task

---
+++ src/br/com/jeansousa/model/Task.java
@@ -0,0 +1,13 @@
+package br.com.jeansousa.model;
+
+public class Task {
+
+
+    // Variáveis do projeto
+    private int id;     // Identificador único
+    private String title;   // Texto com titulo da tarefa
+    private String description;     // Texto com a descrição da tarefa
+    private boolean done;       // Verdadeiro ou Falso indicando se a tarefa esta feita ou não
+
+
+}

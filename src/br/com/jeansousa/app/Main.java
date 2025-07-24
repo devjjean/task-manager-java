d69006e27cf7896b9134927c392eec5cf227539d
Jean Sousa <jjeanssousa16@gmail.com>
2025-07-24 11:01:47 -0300

    Cria a primeira tarefa e exibe as informações com ID automático, Título, Descrição e se está concluída ou não

--- src/br/com/jeansousa/app/Main.java
+++ src/br/com/jeansousa/app/Main.java
@@ -5,7 +5,13 @@ import br.com.jeansousa.model.Task;
 public class Main {
     public static void main(String[] args) {
 
+    // Criação da Tarefa
+    Task FirstTask = new Task("Acadêmia", "Treinar 1:30", false);
 
-
+    // Mostrar os dados da tarefa criada
+     System.out.println("id: " + FirstTask.getId());
+     System.out.println("Título: " + FirstTask.getTitle());
+     System.out.println("Descrição: " + FirstTask.getDescription());
+     System.out.println("Concluído? " + (FirstTask.isDone() ? "Sim" : "Não"));
     }
 }

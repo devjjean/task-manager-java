aa86b15348ed5ef59e878a62f2155ce48c5105c6
Jean Sousa <jjeanssousa16@gmail.com>
2025-07-24 10:19:04 -0300

    Criar o Construtor da entidade Task

--- src/br/com/jeansousa/model/Task.java
+++ src/br/com/jeansousa/model/Task.java
@@ -9,4 +9,11 @@ public class Task {
     private String description;     // Texto com a descrição da tarefa
     private boolean done;       // Verdadeiro ou Falso indicando se a tarefa esta feita ou não
 
+    // Construtor da Task
+    public Task(int id, String title, String description, boolean done) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.done = done;
+    }
 }
